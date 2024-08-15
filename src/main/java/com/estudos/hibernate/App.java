9820a696272a01439f5567b8757c985d32333b50
NEO <NEO@DESKTOP-HJK7C8F>
2024-08-15 19:54:14 -0300

    Crud - C

--- src/main/java/com/estudos/hibernate/App.java
+++ src/main/java/com/estudos/hibernate/App.java
@@ -8,35 +8,18 @@ import org.hibernate.Session;
 import com.estudos.model.Address;
 import com.estudos.model.HibernateSession;
 import com.estudos.model.Options;
+import com.estudos.model.Product;
 import com.estudos.model.User;
 
 public class App {
     public static void main( String[] args ) {
     	Session session = HibernateSession.getSessionFactory().openSession();
     	
-    	User user = new User();
-    	Address address = new Address();
-    	Options optionsInst = new Options();
+    	Product product = new Product();
     	
-    	optionsInst.setName("dklada");
-    	List<Options> options = new ArrayList<Options>();
+    	product.setName("P 1");
+    	product.setPrice(10.00);
     	
-    	options.add(optionsInst);
-    	
-    	address.setNumber(900);
-    	address.setStreet("Down town");
-    	
-    	user.setUsername("Nando");
-    	user.setName("Fernando");
-    	user.setLastname("Sousa");
-    	
-    	address.setUser(user);
-    	user.setAddress(address);
-    	user.setOptions(options);
-    	
-    	System.out.println( "Hello World!" );
-        System.out.println(session);
-        System.out.println(user.getName() + " " + user.getLastname());
-        System.out.println(user.getAddress().getStreet());
+    	session.save(product);
     }
 }
